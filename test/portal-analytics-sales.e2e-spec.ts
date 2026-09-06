888911b74f7ebd05fa8904c2281af64d4f46f767
Jess Mark Baguio <jsmrkbaguio.official@gmail.com>
2026-09-06 15:34:55 +0800

    style: quiet the any-typed lint rules in the sales analytics e2e spec

--- test/portal-analytics-sales.e2e-spec.ts
+++ test/portal-analytics-sales.e2e-spec.ts
@@ -7,6 +7,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import { INestApplication, ValidationPipe, HttpStatus } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
